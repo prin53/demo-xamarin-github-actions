e21c1591fb4f63a5aff43334c6a2625c35439655
Denys Fiediaiev <fiediaiev@sbyte.dev>
2021-09-19 17:04:56 +0300

    Add sample project

---
+++ GitHubActions.Android/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+
+namespace GitHubActions.Droid
+{
+    [Activity(Label = "GitHubActions", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class MainActivity : FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
+            Forms.Init(this, savedInstanceState);
+
+            LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ GitHubActions.iOS/AppDelegate.cs
@@ -0,0 +1,25 @@
+﻿using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+namespace GitHubActions.iOS
+{
+    [Register(nameof(AppDelegate))]
+    public partial class AppDelegate : FormsApplicationDelegate
+    {
+        private static void Main(string[] args)
+        {
+            UIApplication.Main(args, null, nameof(AppDelegate));
+        }
+
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            Forms.Init();
+
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
